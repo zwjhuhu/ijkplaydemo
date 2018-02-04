f411bfaa24e3acfdbe05c0941c8f159f8714559c
wenjun.zhou <wenjun.zhou@qlchat.com>
2018-02-04 21:34:46 +0800

    ijkplayerdemo

---
+++ ijkdemo/app/src/main/java/zwj/net/ijkdemo/MyApplication.java
@@ -0,0 +1,17 @@
+package zwj.net.ijkdemo;
+
+import android.app.Application;
+import android.content.pm.PackageManager;
+import android.os.Build;
+
+/**
+ * Created by Administrator on 2018/2/4.
+ */
+
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
